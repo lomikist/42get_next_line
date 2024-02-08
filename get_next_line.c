0de22a312d0c931c68bc174e342cd13f6abdb45e
Arman Sargsyan <arsargsy@c1r4s2.42yerevan.am>
2024-02-08 22:22:28 +0400

    tail_str generation part (static)

--- get_next_line.c
+++ get_next_line.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 20
+#define BUFFER_SIZE 30
 
 size_t	ft_strlen(const char *s)
 {
@@ -14,20 +14,64 @@ size_t	ft_strlen(const char *s)
 		length++;
 	return (length);
 }
-char	*ft_strcpy(char *dst, const char *src)
-{
-	int		i;
-	char	*tmp;
 
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+	char			*str;
+
+	str = (char *)s;
 	i = 0;
-	tmp = dst;
-	while (src[i])
+	while (str[i] != '\0')
 	{
-		tmp[i] = src[i];
+		if (str[i] == (char)c)
+			return (&str[i]);
 		i++;
 	}
-	tmp[i] = '\0';
-	return (tmp);
+	if (str[i] == (char)c)
+		return (&str[i]);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	unsigned int	str_len;
+	char			*substr;
+
+	if (!s)
+		return (0);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		len = 0;
+	if (len > str_len - start)
+		len = str_len - start;
+	substr = malloc(len + 1);
+	if (!substr)
+		return (0);
+	i = 0;
+	while (i < len && s[start])
+	{
+		substr[i] = s[start];
+		start++;
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 char	*ft_strcat(char *s1, const char *s2)
@@ -47,19 +91,7 @@ char	*ft_strcat(char *s1, const char *s2)
 	return (s1);
 }
 
-
-int chack_if_new_line(char *str)
-{
-	while (*str != '\0')
-	{
-		if (*str == '\n')
-			return 1;
-		str++;
-	}
-	return (0);
-}
-
-char *get_next_line(int fd)
+char	*read_one_line(int fd)
 {
 	char	*str_read;
 	char	*str;
@@ -69,7 +101,7 @@ char *get_next_line(int fd)
 	buffer_counter = 1;
 	str_read = malloc(sizeof(char) * (BUFFER_SIZE * buffer_counter));
 	read(fd, str_read, BUFFER_SIZE);
-	while (!chack_if_new_line(str_read))
+	while (!ft_strchr(str_read, '\n'))
 	{
 		if (str)
 		{
@@ -86,6 +118,23 @@ char *get_next_line(int fd)
 		read(fd, str_read, BUFFER_SIZE);
 		str = ft_strcat(str, str_read);
 	}
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*str;
+	const char	*full_str;
+	static char	*tail_str;
+	int			finded_index;
+	int			full_str_len;
+
+	full_str = (const char *)read_one_line(fd);
+	finded_index = ft_strchr(full_str, '\n') - &full_str[0];
+	full_str_len = ft_strlen(full_str);
+	str = ft_substr(full_str, 0, finded_index);
+	tail_str = ft_substr(full_str, finded_index + 1, full_str_len - 2 - finded_index);
+
 	return (str);
 }
 
