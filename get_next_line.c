90e4698c95fd213d96ebc01eec7c36637bf45450
Arman Sargsyan <arsargsy@c1r1s4.42yerevan.am>
2024-02-23 00:51:37 +0400

    last

--- get_next_line.c
+++ get_next_line.c
@@ -1,93 +1,70 @@
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char *s1, const char *s2);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
-
-char	*read_one_line(int fd)
+char	*read_one_line(char *str, int fd)
 {
 	char	str_read[BUFFER_SIZE + 1];
-	char	*str = NULL;
 	int 	count = 1;
-	int		i;
 
-	i = -1;
-	while (++i <= BUFFER_SIZE)
-		str_read[i] = '\0';
-
-	while ((count > 0) && (!ft_strchr(str_read, '\n')))
+	count = -1;
+	while (++count <= BUFFER_SIZE)
+		str_read[count] = '\0';
+	count = 1;
+	while (!ft_strchr(str_read, '\n') && count)
 	{
 		count = read(fd, str_read, BUFFER_SIZE);
-		if (count < 0)
-			return (str);
+		if (count < 0 || (!count && !str))
+			return (NULL);
 		str_read[count] = '\0';
 		if (str)
 			str = ft_strjoin(str, str_read);
 		else
 			str = ft_strdup(str_read);
 		if (!str)
-			return NULL;
+			return (NULL);
 	}
 	return (str);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*str = NULL;
-	const char	*full_str = NULL;
 	static char	*tail_str;
-	int			finded_index;
-	int			full_str_len;
-	char 		*str_read;
+	int			i;
+	char 		*str;
+	char		*temp;	
 
-	if (tail_str)
-	{
-		str_read = read_one_line(fd);
-		if (str_read)
-			full_str = ft_strjoin(tail_str, str_read);
-		else 
-			full_str = ft_strdup(tail_str);
-		finded_index = ft_strchr(full_str, '\n') - &full_str[0] + 1;
-		full_str_len = ft_strlen(full_str);
-		str = ft_substr(full_str, 0, finded_index);
-		tail_str = ft_substr(full_str, finded_index, full_str_len - finded_index);
-		free(str_read);
-	}
-	else 
-	{
-		full_str = (const char *)read_one_line(fd);
-		int full_str_len = ft_strlen(full_str);
-		finded_index = ft_strchr(full_str, '\n') - &full_str[0] + 1;
-		full_str_len = ft_strlen(full_str);
-		str = ft_substr(full_str, 0, finded_index);
-		tail_str = ft_substr(full_str, finded_index, full_str_len - finded_index);
-	}
-	free((char *)full_str);
+	i = 0;
+	tail_str = read_one_line(tail_str, fd);
+	if (!tail_str)
+		return (NULL);
+	while (tail_str[i] != '\n' && tail_str[i + 1])
+		i++;
+	str = ft_substr(tail_str, 0, i + 1);
+	temp = tail_str;
+	tail_str = ft_substr(tail_str, i + 1, ft_strlen(tail_str) - i - 1);
+	free(temp);
 	return (str);
 }
 
-int main()
-{
-	int fd = open("test", O_RDONLY);
-	char *a = read_one_line(fd);
-	char *b = get_next_line(fd);
-	// char *c = get_next_line(fd);
-	// char *a1 = get_next_line(fd);
-	// char *b1 = get_next_line(fd);
-	// char *c1 = get_next_line(fd);
-	// char *d = get_next_line(fd);
-	// char *e = get_next_line(fd);
-	puts(a);
-	// puts(b);
-	// puts(c);
-	// puts(a1);
-	// puts(b1);
-	// puts(c1);
-	// puts(d);
-	// puts(e);
-	// free(a);
-	// free(b);
-	// system("leaks a.out");
-}
+// int main()
+// {
+// 	int fd = open("test", O_RDONLY);
+// 	char *a = get_next_line(fd);
+// 	// char *b = get_next_line(fd);
+// 	// char *c = get_next_line(fd);
+// 	// char *a1 = get_next_line(fd);
+// 	// char *b1 = get_next_line(fd);
+// 	// char *c1 = get_next_line(fd);
+// 	// char *d = get_next_line(fd);
+// 	// char *e = get_next_line(fd);
+// 	puts(a);
+// 	// puts(b);
+// 	// puts(c);
+// 	// puts(a1);
+// 	// puts(b1);
+// 	// puts(c1);
+// 	// puts(d);
+// 	// puts(e);
+// 	// free(a);
+// 	// free(b);
+// 	// system("leaks a.out");
+// }
